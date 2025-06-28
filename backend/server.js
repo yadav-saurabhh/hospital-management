36e90a786fe0edbfa645c895105f12fc12b11284
Saurabh Yadav <syadav02818@gmail.com>
2025-06-28 10:29:30 +0530

    update cors setting in backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from 'cors'
+// import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
