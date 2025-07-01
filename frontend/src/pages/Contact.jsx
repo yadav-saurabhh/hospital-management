1cdc373ebe608f315e7a48422351def76c95f40d
yadav-saurabh <syadav02818@gmail.com>
2025-07-01 18:35:46 +0530

    new changes in contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -14,8 +14,8 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className=' text-gray-500'>B2/50A, Bhadaini, <br />Tulsi Ghat, Assi Road(Heritage varanasi)</p>
-          <p className=' text-gray-500'>Tel: 9621893109 <br /> Email: syadav02818@gmail.com
-          Tel: 8081679626 <br /> Email: akashpro8585@gmail.com
+          <p className=' text-gray-500'>Tel: 9621893109 <br /> Email: syadav02818@gmail.com <br />
+          Tel: 8081679626 <br /> Email: akashpro8585@gmail.com <br />
           Tel: 9956571969 <br /> Email: prashantchaubey051@gmail.com
           </p>
           <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
